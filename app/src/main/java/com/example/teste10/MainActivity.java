4d098bb9ee9551a691acd5284214dfe1c47b9f44
CCASTROINFO26 <claitonycastro@gmail.com>
2019-12-19 11:47:20 -0300

    Modificações novas

--- app/src/main/java/com/example/teste10/MainActivity.java
+++ app/src/main/java/com/example/teste10/MainActivity.java
@@ -5,8 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +18,4 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, Cadastro.class);
         startActivity(intent);
     }
-    //Spinner spinner = {Spinner} findViewById
 }
